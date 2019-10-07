a2d893188b3c3f2519d5d2bea9d9e6e7d7f6c819
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-07 13:25:44 -0300

    Componente Header

---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './style.css';
+
+/**
+ * Conceito Stateless Component
+ */
+const Header = () => <header id="main-header">JSHunt</header>;
+
+export default Header
