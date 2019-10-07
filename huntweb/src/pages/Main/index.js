261a799a74104932d9ae9a0706175c0b96c73b87
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-07 15:54:57 -0300

    Paginação

--- huntweb/src/pages/Main/index.js
+++ huntweb/src/pages/Main/index.js
@@ -6,7 +6,8 @@ import './style.css';
 export default class Main extends Component {
     state = {
         products: [],
-        paginate: {}
+        paginate: {},
+        page: 1
     }
     /**
      * Função nativa do ReactJS
@@ -16,23 +17,39 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
+    prevPage = () => {
+        const { page } = this.state;
+        if (page === 1) return;
+
+        const currentPage = page - 1;
+        this.loadProducts(currentPage);
+    }
+
+    nextPage = () => {
+        const { page, paginate } = this.state;
+        if (page === paginate.pages) return;
+
+        const currentPage = page + 1;
+        this.loadProducts(currentPage);
+    }
+
     /**
      * Função própria criada pelo dev
      * Consome o endereço api recebendo um objeto de itens
      * Setando as propriedade do State
      */
-    loadProducts = async () => {
-        const response = await api.get(`/products`);
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         const { docs, ...paginate } = response.data;
 
-        this.setState({ products: docs, paginate });
+        this.setState({ products: docs, paginate, page });
     }
     /**
      * Função nativa do ReactJS
      * Que da o Start do app
      */
     render() {
-        const { products } = this.state;
+        const { products, paginate, page } = this.state;
 
         return (
             <div className='products-list'>
@@ -40,9 +57,14 @@ export default class Main extends Component {
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a href="">Acessar</a>
+                        {/* <a href="">Acessar</a> */}
                     </article>
                 ))}
+                <div className="pagination">
+                    <button disabled={ page === 1 } onClick={ this.prevPage }>Anterior</button>
+                    <p>{`Página ${page}/${paginate.pages}`}</p>
+                    <button disabeld={ page === paginate.pages } onClick={ this.nextPage }>Próximo</button>
+                </div>
             </div>
         );
     }
