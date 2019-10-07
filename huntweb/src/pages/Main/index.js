1efd753077babaffdea0613b7d7c5c8085f2bd59
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-07 14:57:07 -0300

    Alguns comentarios nas funções

--- huntweb/src/pages/Main/index.js
+++ huntweb/src/pages/Main/index.js
@@ -8,32 +8,41 @@ export default class Main extends Component {
         products: [],
         paginate: {}
     }
-
+    /**
+     * Função nativa do ReactJS
+     * Utilizada assim que o render é iniciado
+     */
     componentDidMount() {
         this.loadProducts();
     }
 
+    /**
+     * Função própria criada pelo dev
+     * Consome o endereço api recebendo um objeto de itens
+     * Setando as propriedade do State
+     */
     loadProducts = async () => {
         const response = await api.get(`/products`);
         const { docs, ...paginate } = response.data;
 
         this.setState({ products: docs, paginate });
     }
-
+    /**
+     * Função nativa do ReactJS
+     * Que da o Start do app
+     */
     render() {
         const { products } = this.state;
 
         return (
             <div className='products-list'>
-                {
-                    products.map(product => (
-                        <article key={product._id}>
-                            <strong>{product.title}</strong>
-                            <p>{product.description}</p>
-                            <a href="">Acessar</a>
-                        </article>
-                    ))
-                }
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
             </div>
         );
     }
